0ebba5638b9a5c3742a491b9da1f6d6e2174faa6
Yogabayu <yogabayusbi@gmail.com>
2024-01-01 12:40:26 +0700

    v1.1.4-guest-init | update

---
+++ public/symlink.php
@@ -0,0 +1,7 @@
+<?php
+
+$target  = '/home/n1662513/public_html/ecommerce.yogabayuap.com/storage/app/public';
+
+$link    = '/home/n1662513/public_html/ecommerce.yogabayuap.com/public/storage';
+
+symlink($target, $link);
