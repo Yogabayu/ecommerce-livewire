de92181a06baf4d6f916e7749f1331ea1a0935e9
Yogabayu <yogabayusbi@gmail.com>
2024-01-16 09:16:39 +0700

    v1.2-rev | update symlink

--- public/symlink.php
+++ public/symlink.php
@@ -1,7 +1,9 @@
 <?php
 
-$target  = '/home/n1662513/public_html/ecommerce.yogabayuap.com/storage/app/public';
+// $target  = '/home/n1662513/public_html/ecommerce.yogabayuap.com/storage/app/public';
+$target  = '/home/bankarthaya/infolelang.bankarthaya.com/storage/app/public';
 
-$link    = '/home/n1662513/public_html/ecommerce.yogabayuap.com/public/storage';
+// $link    = '/home/n1662513/public_html/ecommerce.yogabayuap.com/public/storage';
 
+$link    = '/home/bankarthaya/infolelang.bankarthaya.com/public/storage';
 symlink($target, $link);
