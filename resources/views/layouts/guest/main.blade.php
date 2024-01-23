d4c1afa3d733814741162f153eebbe2ab78a6fb0
Yogabayu <yogabayusbi@gmail.com>
2024-01-23 15:11:49 +0700

    v1.2 | add favicon

--- resources/views/layouts/guest/main.blade.php
+++ resources/views/layouts/guest/main.blade.php
@@ -12,6 +12,9 @@
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('storage/public/setting/' . $setting->logo) }}" type="image/x-icon">
+
     @stack('style')
     <style>
         .footer-logo {
