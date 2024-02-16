045ed83d65cf336ae5b162582e3fff28695a521e
Yogabayu <yogabayusbi@gmail.com>
2024-02-16 11:00:42 +0700

    v1.2-rev |bismillah

--- app/Livewire/SearchComponentInline.php
+++ app/Livewire/SearchComponentInline.php
@@ -65,6 +65,119 @@ class SearchComponentInline extends Component
 
     public function search($value)
     {
+        // $query = DB::table('products as p')
+        //     ->join('detail_products as dp', 'p.id', '=', 'dp.product_id')
+        //     ->join('product_photos as pp', 'p.id', '=', 'pp.product_id')
+        //     ->join('product_tag_mappings as ptm', 'p.id', '=', 'ptm.product_id')
+        //     ->leftJoin('auction_schedules as ac', 'p.id', '=', 'ac.product_id')
+        //     ->join('categories as c', 'c.id', '=', 'p.category_id')
+        //     ->join('tags as t', 'ptm.tag_id', '=', 't.id')
+        //     ->select(
+        //         'p.id',
+        //         'p.name',
+        //         'p.slug',
+        //         'p.price',
+        //         'dp.after_sale',
+        //         'dp.seeing_count',
+        //         'dp.share_count',
+        //         'c.name as nameCategory',
+        //         'pp.photo'
+        //     )
+        //     ->where('pp.is_primary', 1)
+        //     ->where('p.publish', '!=', 0);
+
+        // if ($this->category && $this->tag == '' && $this->inputText == '') {
+        //     $category = $this->category;
+        //     // $query->where(function ($query) use ($category) {
+        //     //     $query->where('c.name', 'LIKE', '%' . $category . '%')
+        //     //         ->orWhere('c.slug', 'LIKE', '%' . $category . '%');
+        //     // });
+        //     $idCategory = DB::table('categories')->where('name', 'LIKE', '%' . $category . '%')->first();
+        //     $query->where('c.id', $idCategory->id);
+        // } elseif ($this->tag && $this->category == '' && $this->inputText == '') {
+        //     $tag = $this->tag;
+        //     $query->where(function ($query) use ($tag) {
+        //         $query->where('t.name', $tag);
+        //     });
+        // } elseif ($this->lowPrice || $this->highPrice) {
+        //     $low = str_replace(".", "", $this->lowPrice);
+        //     $high = str_replace(".", "", $this->highPrice);
+
+        //     $query->where(function ($query) use ($low, $high) {
+        //         if (
+        //             $low != null
+        //         ) {
+        //             $query->whereRaw('CAST(REPLACE(p.price, ".", "") AS UNSIGNED) >= ?', [(int)$low]);
+        //         }
+
+        //         if ($high != null) {
+        //             $query->whereRaw('CAST(REPLACE(p.price, ".", "") AS UNSIGNED) <= ?', [(int)$high]);
+        //         }
+        //     });
+        // } elseif ($this->inputText) {
+        //     $inputTextWithoutSpaces = str_replace(' ', '', $this->inputText);
+        //     $slug = str_replace(' ', '-', $this->inputText);
+        //     $inputText = $this->inputText;
+
+        //     $query->where(function ($query) use ($inputTextWithoutSpaces, $slug, $inputText) {
+        //         $query->where('p.name', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere('p.short_desc', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere('dp.long_desc', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere('dp.address', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere('c.name', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere('p.slug', 'LIKE', '%' . $slug . '%')
+        //             ->orWhere('c.slug', 'LIKE', '%' . $slug . '%')
+        //             ->orWhere('t.name', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere('ac.kpknl', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere('p.price', 'LIKE', '%' . $inputText . '%')
+        //             ->orWhere(DB::raw("REPLACE(p.name, ' ', '')"), 'LIKE', '%' . $inputTextWithoutSpaces . '%')
+        //             ->orWhere(DB::raw("REPLACE(p.short_desc, ' ', '')"), 'LIKE', '%' . $inputTextWithoutSpaces . '%')
+        //             ->orWhere(DB::raw("REPLACE(dp.long_desc, ' ', '')"), 'LIKE', '%' . $inputTextWithoutSpaces . '%')
+        //             ->orWhere(DB::raw("REPLACE(c.name, ' ', '')"), 'LIKE', '%' . $inputTextWithoutSpaces . '%');
+        //     });
+        // }
+
+        // if ($value == 1) { // Terbaru
+        //     $query->orderBy('p.created_at', 'DESC');
+        // } elseif ($value == 2) { // Terlama
+        //     $query->orderBy('p.created_at', 'ASC');
+        // } elseif ($value == 3) { // A-Z
+        //     $query->orderBy('p.name', 'ASC');
+        // } elseif ($value == 4) { // Z-A
+        //     $query->orderBy('p.name', 'DESC');
+        // } elseif ($value == 5) { // Termahal
+        //     // $query->orderBy(DB::raw('CAST(REPLACE(p.price, ".", "") AS SIGNED)'), 'DESC');
+        //     $query->orderBy(DB::raw('
+        //         CASE
+        //             WHEN dp.after_sale IS NOT NULL THEN CAST(REPLACE(dp.after_sale, ".", "") AS SIGNED)
+        //             ELSE CAST(REPLACE(p.price, ".", "") AS SIGNED)
+        //         END
+        //     '), 'DESC');
+        // } elseif ($value == 6) { // Termurah
+        //     // $query->orderBy(DB::raw('CAST(REPLACE(p.price, ".", "") AS SIGNED)'), 'ASC');
+        //     $query->orderBy(DB::raw('
+        //         CASE
+        //             WHEN dp.after_sale IS NOT NULL THEN CAST(REPLACE(dp.after_sale, ".", "") AS SIGNED)
+        //             ELSE CAST(REPLACE(p.price, ".", "") AS SIGNED)
+        //         END
+        //     '), 'ASC');
+        // }
+
+        // $this->results = $query->groupBy(
+        //     'c.name',
+        //     'p.id',
+        //     'p.name',
+        //     'p.slug',
+        //     'p.short_desc',
+        //     'p.price',
+        //     'dp.after_sale',
+        //     'dp.seeing_count',
+        //     'dp.share_count',
+        //     'pp.photo'
+        // )->simplePaginate(10);
+
+        // $this->countProduct = $query->count();
+
         $query = DB::table('products as p')
             ->join('detail_products as dp', 'p.id', '=', 'dp.product_id')
             ->join('product_photos as pp', 'p.id', '=', 'pp.product_id')
@@ -104,9 +217,7 @@ class SearchComponentInline extends Component
             $high = str_replace(".", "", $this->highPrice);
 
             $query->where(function ($query) use ($low, $high) {
-                if (
-                    $low != null
-                ) {
+                if ($low != null) {
                     $query->whereRaw('CAST(REPLACE(p.price, ".", "") AS UNSIGNED) >= ?', [(int)$low]);
                 }
 
@@ -127,7 +238,11 @@ class SearchComponentInline extends Component
                     ->orWhere('c.name', 'LIKE', '%' . $inputText . '%')
                     ->orWhere('p.slug', 'LIKE', '%' . $slug . '%')
                     ->orWhere('c.slug', 'LIKE', '%' . $slug . '%')
-                    ->orWhere('t.name', 'LIKE', '%' . $inputText . '%')
+                    ->orWhere(
+                        't.name',
+                        'LIKE',
+                        '%' . $inputText . '%'
+                    )
                     ->orWhere('ac.kpknl', 'LIKE', '%' . $inputText . '%')
                     ->orWhere('p.price', 'LIKE', '%' . $inputText . '%')
                     ->orWhere(DB::raw("REPLACE(p.name, ' ', '')"), 'LIKE', '%' . $inputTextWithoutSpaces . '%')
@@ -146,37 +261,24 @@ class SearchComponentInline extends Component
         } elseif ($value == 4) { // Z-A
             $query->orderBy('p.name', 'DESC');
         } elseif ($value == 5) { // Termahal
-            // $query->orderBy(DB::raw('CAST(REPLACE(p.price, ".", "") AS SIGNED)'), 'DESC');
             $query->orderBy(DB::raw('
-                CASE
-                    WHEN dp.after_sale IS NOT NULL THEN CAST(REPLACE(dp.after_sale, ".", "") AS SIGNED)
-                    ELSE CAST(REPLACE(p.price, ".", "") AS SIGNED)
-                END
-            '), 'DESC');
+        CASE
+            WHEN dp.after_sale IS NOT NULL THEN CAST(REPLACE(dp.after_sale, ".", "") AS SIGNED)
+            ELSE CAST(REPLACE(p.price, ".", "") AS SIGNED)
+        END
+    '), 'DESC');
         } elseif ($value == 6) { // Termurah
-            // $query->orderBy(DB::raw('CAST(REPLACE(p.price, ".", "") AS SIGNED)'), 'ASC');
             $query->orderBy(DB::raw('
-                CASE
-                    WHEN dp.after_sale IS NOT NULL THEN CAST(REPLACE(dp.after_sale, ".", "") AS SIGNED)
-                    ELSE CAST(REPLACE(p.price, ".", "") AS SIGNED)
-                END
-            '), 'ASC');
+        CASE
+            WHEN dp.after_sale IS NOT NULL THEN CAST(REPLACE(dp.after_sale, ".", "") AS SIGNED)
+            ELSE CAST(REPLACE(p.price, ".", "") AS SIGNED)
+        END
+    '), 'ASC');
         }
 
-        $this->results = $query->groupBy(
-            'c.name',
-            'p.id',
-            'p.name',
-            'p.slug',
-            'p.short_desc',
-            'p.price',
-            'dp.after_sale',
-            'dp.seeing_count',
-            'dp.share_count',
-            'pp.photo'
-        )->simplePaginate(10);
+        $this->results = $query->simplePaginate(10);
 
-        $this->countProduct = $query->count();
+        $this->countProduct = $query->select('p.id')->distinct()->count();
 
         $this->state = $value;
     }
